5bae462f75e4531783233e5c14513b00eb6bd46e
Alexey Nikitin <nikialeksey@gmail.com>
2020-07-25 00:03:45 +0700

    Allfinal: skip interface method params

--- plugin-maven/src/main/java/com/iwillfailyou/AllfinalSettings.java
+++ plugin-maven/src/main/java/com/iwillfailyou/AllfinalSettings.java
@@ -5,24 +5,36 @@ import java.util.List;
 
 public final class AllfinalSettings {
     private final List<Integer> threshold;
+    private final List<Boolean> skipInterfaceMethodParams;
 
     public AllfinalSettings() {
         this(
-            Arrays.asList(0)
+            Arrays.asList(0),
+            Arrays.asList(true)
         );
     }
 
     public AllfinalSettings(
-        final List<Integer> threshold
+        final List<Integer> threshold,
+        final List<Boolean> skipInterfaceMethodParams
     ) {
         this.threshold = threshold;
+        this.skipInterfaceMethodParams = skipInterfaceMethodParams;
     }
 
     public int getThreshold() {
         return threshold.get(0);
     }
 
-    public int setThreshold(final int threshold) {
-        return this.threshold.set(0, threshold);
+    public void setThreshold(final int threshold) {
+        this.threshold.set(0, threshold);
+    }
+
+    public boolean getSkipInterfaceMethodParams() {
+        return skipInterfaceMethodParams.get(0);
+    }
+
+    public void setSkipInterfaceMethodParams(final boolean skipInterfaceMethodParams) {
+        this.skipInterfaceMethodParams.set(0, skipInterfaceMethodParams);
     }
 }
